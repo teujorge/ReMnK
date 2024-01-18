1491a5169f4833ac2aa1dfe8708f6047dccce80f
Matheus Jorge <mrljorge@outlook.com>
2024-01-18 19:59:44 -0300

    add : no mouse event handler + cleanup

--- tauri/src/controller.rs
+++ tauri/src/controller.rs
@@ -1,21 +1,23 @@
-use std::{
-    sync::{Arc, Mutex},
-    time::Instant,
-};
+use std::sync::{Arc, Mutex};
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use tauri::Manager;
+
+use tauri::AppHandle;
+
+const US_TO_S: f64 = 1.0 / 1000000.0; // Microseconds to seconds multiplier
+const STOP_THRESHOLD: f64 = 0.5; // Threshold for detecting stop in seconds
+const MAX_SPEED: f64 = 1000.0; // Max speed in pixels per second
+const DEFAULT_R3: &str = "r3_0_0"; // Default R3 command
 
 pub struct MouseEventData {
-    x: f64,
-    y: f64,
-    timestamp: Instant,
+    x: f64,    // in pixels
+    y: f64,    // in pixels
+    time: f64, // in microseconds
 }
 
 impl MouseEventData {
-    pub fn new(x: f64, y: f64) -> Self {
-        MouseEventData {
-            x,
-            y,
-            timestamp: Instant::now(),
-        }
+    pub fn new(x: f64, y: f64, time: f64) -> Self {
+        MouseEventData { x, y, time }
     }
 }
 
@@ -106,11 +108,12 @@ pub fn virtualize_controller(
                 let mouse_move = mouse_move.unwrap();
                 let x = mouse_move["x"].as_f64().unwrap();
                 let y = mouse_move["y"].as_f64().unwrap();
+                let time = mouse_move["time"].as_f64().unwrap();
 
                 // Lock the mutex to safely access and update the shared state
                 let mut prev_mouse_event = mutex_prev_mouse_event_data.lock().unwrap();
 
-                let new_mouse_event = MouseEventData::new(x, y);
+                let new_mouse_event = MouseEventData::new(x, y, time);
 
                 // Calculate the controller movement based on the current and previous mouse data
                 let controller_command =
@@ -155,34 +158,80 @@ pub fn virtualize_controller(
 fn translate_mouse_to_controller(current: &MouseEventData, previous: &MouseEventData) -> String {
     let delta_x = current.x - previous.x;
     let delta_y = current.y - previous.y;
-    let time_elapsed = current
-        .timestamp
-        .duration_since(previous.timestamp)
-        .as_secs_f64();
 
-    if time_elapsed <= 0.0 {
-        // {x: 0, y: 0}
-        return "r3_0_0".to_owned();
+    let delta_time_secs = (current.time - previous.time) * US_TO_S;
+
+    if delta_time_secs > STOP_THRESHOLD {
+        // Mouse stopped
+        return DEFAULT_R3.to_owned();
     }
-    // adjust the scaling factor as needed ...
 
     let delta_magnitude = (delta_x * delta_x + delta_y * delta_y).sqrt();
-    let speed_scalar = delta_magnitude / time_elapsed;
+
+    if delta_magnitude <= 0.0 {
+        // No significant movement
+        return DEFAULT_R3.to_owned();
+    }
+
+    let speed_scalar = (delta_magnitude / delta_time_secs).min(MAX_SPEED);
     let direction_vector = vec![delta_x / delta_magnitude, delta_y / delta_magnitude];
 
     // println!(
     //     "translate_mouse_to_controller: {}  |  {} ms",
     //     format!(
-    //         "r3 | x%{} | y%{}",
+    //         "r3 | x{} | y{}",
     //         (direction_vector[0] * speed_scalar).floor(),
     //         (direction_vector[1] * speed_scalar).floor(),
     //     ),
-    //     (time_elapsed * 1000.0).floor()
+    //     (delta_time_secs).floor()
     // );
 
     return format!(
         "r3_{}_{}",
-        direction_vector[0] * speed_scalar,
-        direction_vector[1] * speed_scalar,
+        (direction_vector[0] * speed_scalar).floor(),
+        (direction_vector[1] * speed_scalar).floor()
     );
+}
+
+pub fn handle_no_mouse_events(
+    handle: &AppHandle,
+    mutex_prev_mouse_event_data: Arc<Mutex<MouseEventData>>,
+) {
+    let debounce_duration = Duration::from_millis(100); // example debounce time
+    let inactivity_threshold = Duration::from_millis(100); // threshold for inactivity
+
+    loop {
+        std::thread::sleep(debounce_duration);
+
+        let current_time = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards")
+            .as_micros() as f64;
+
+        let prev_mouse_event = mutex_prev_mouse_event_data.lock().unwrap();
+
+        let inactivity_threshold_ms = inactivity_threshold.as_micros() as f64;
+        let time_since_last_event = current_time - prev_mouse_event.time;
+
+        // Check if the time since the last event FAR exceeds the threshold
+        // This is to prevent the controller from being spammed with default events
+        if time_since_last_event > inactivity_threshold_ms * 10.0 {
+            continue;
+        }
+
+        // Check if the time since the last event exceeds the threshold
+        if current_time - prev_mouse_event.time <= inactivity_threshold_ms {
+            continue;
+        }
+
+        // println!(
+        //     "No mouse events for {} ms",
+        //     (current_time - prev_mouse_event.time) / 1000.0,
+        // );
+
+        // Send a default controller event to center the controller joystick
+        handle
+            .emit_all("controller-event", DEFAULT_R3)
+            .expect("Failed to emit controller-event");
+    }
 }
--- tauri/src/main.rs
+++ tauri/src/main.rs
@@ -6,7 +6,6 @@ mod controller;
 mod ipc;
 
 // Import necessary crates
-use crate::controller::{virtualize_controller, MouseEventData};
 use std::process::{Child, Command};
 use std::sync::mpsc::{self, Receiver, Sender};
 use std::sync::{Arc, Mutex};
@@ -18,7 +17,8 @@ fn main() {
     let (tx, rx): (Sender<String>, Receiver<String>) = mpsc::channel();
 
     // Create a mutex for the previous mouse event
-    let mutex_prev_mouse_event_data = Arc::new(Mutex::new(MouseEventData::new(0.0, 0.0)));
+    let mutex_prev_mouse_event_data =
+        Arc::new(Mutex::new(controller::MouseEventData::new(0.0, 0.0, 0.0)));
 
     // Run IPC server in a separate thread
     thread::spawn(move || {
@@ -38,26 +38,49 @@ fn main() {
     tauri::Builder::default()
         .manage(mnk_process)
         // Setup Tauri app to receive messages from the listener process and send them to FE
-        .setup(|app| {
-            let handle = app.handle();
+        .setup(move |app| {
+            let handle_for_middleman = app.handle();
+            let handle_for_no_mouse_event = app.handle();
 
-            println!("Tauri setup -> spawn thread");
+            let mutex_prev_mouse_event_data_clone_for_middleman =
+                mutex_prev_mouse_event_data.clone();
+            let mutex_prev_mouse_event_data_clone_for_no_mouse_event =
+                mutex_prev_mouse_event_data.clone();
+
+            println!("Tauri setup -> spawn mnk middleman thread");
             // Use the `rx` here in the main thread to receive messages
             std::thread::spawn(move || {
                 for received in rx {
                     // println!("tauri to send data to FE: {}", received);
 
                     // Send the received message to the FE as mnk-event
-                    handle
+                    handle_for_middleman
                         .emit_all("mnk-event", &received)
                         .expect("Failed to emit mnk-event");
 
+                    // Virtualize the controller based on the received message
+                    let controller_event = controller::virtualize_controller(
+                        &received,
+                        &mutex_prev_mouse_event_data_clone_for_middleman,
+                    );
+
                     // Send the received message to the FE as controller-event
-                    let controller_event =
-                        virtualize_controller(&received, &mutex_prev_mouse_event_data);
-                    handle
+                    handle_for_middleman
                         .emit_all("controller-event", &controller_event)
                         .expect("Failed to emit controller-event");
+                }
+            });
+
+            println!("Tauri setup -> spawn no mouse events thread");
+            // Spawn a thread to handle no mouse events
+            std::thread::spawn({
+                let mutex_prev_mouse_event_data_clone =
+                    mutex_prev_mouse_event_data_clone_for_no_mouse_event.clone();
+                move || {
+                    controller::handle_no_mouse_events(
+                        &handle_for_no_mouse_event,
+                        mutex_prev_mouse_event_data_clone,
+                    );
                 }
             });
 
